ce60b00fc7b2ea63092fd3decdb7dfef125d2f2c
J_sky <bosichong@qq.com>
2020-03-09 00:32:54 +0800

    完成了最基本的正则功能

--- src/components/App.vue
+++ src/components/App.vue
@@ -31,19 +31,30 @@
     </v-app-bar>
 
     <v-content>
-      
-        <div class="d-block">
-          <v-textarea type="text"  label="待匹配的文本" v-model="textSour" @click="reptest"></v-textarea>
-        </div>
+      <div class="d-block">
+        <v-textarea type="text" label="待匹配的文本" v-model:value="textSour" @input="reptest"></v-textarea>
+      </div>
 
-        <div class="d-block">
-          <v-text-field type="text" label="正则表达式" v-model="textPattern"></v-text-field>
-        </div>
+      <div class="d-block">
+        <v-row>
+          <v-col cols="12" md="9">
+            <v-text-field type="text" label="正则表达式" v-model="textPattern" @input="reptest"></v-text-field>
+          </v-col>
+          <v-col cols="12" md="1">
+            <v-checkbox v-model="rep_g" label="G" required></v-checkbox>
+          </v-col>
+          <v-col cols="12" md="1">
+            <v-checkbox v-model="rep_i" label="I" required></v-checkbox>
+          </v-col>
+          <v-col cols="12" md="1">
+            <v-checkbox v-model="rep_m" label="M" required></v-checkbox>
+          </v-col>
+        </v-row>
+      </div>
 
-        <div class="d-block">
-          <v-textarea type="text" label="匹配结果" ></v-textarea>
-        </div>
-      
+      <div class="d-block">
+        <v-textarea type="text" label="匹配结果" v-model="textMatchResult"></v-textarea>
+      </div>
     </v-content>
 
     <v-footer app>
@@ -60,14 +71,44 @@ export default {
 
   data: () => ({
     drawer: false,
-    textSour:"",
-    textPattern:""
-    
+    textSour: "",
+    textPattern: "",
+    textMatchResult: "",
+    rep_g: true,
+    rep_i: false,
+    rep_m: false
   }),
-  methods:{
-      reptest:function(){
-          alert("hello")
+  methods: {
+    reptest: function() {
+      var op = ""; //组装正则表达式修饰符
+      if (this.rep_g) op = "g";
+      if (this.rep_i) op = op + "i";
+      if (this.rep_m) op = op + "m";
+      console.log(this.textSour);
+      
+      var regex = new RegExp(this.textPattern, op); //创建RegExp对象。
+      var result = this.textSour.match(regex);
+      console.log(result);
+      
+
+      // 如果没有匹配提示信息
+      if (null == result || 0 == result.length) {
+        this.textMatchResult = "（没有匹配）";
+        return false;
       }
+
+      if (this.rep_g) {
+        var strResult = "共找到 " + result.length + " 处匹配：\r\n";
+        for (var i = 0; i < result.length; ++i){
+            strResult = strResult + result[i] + "\r\n";
+        }
+        
+        this.textMatchResult = strResult;
+      } else {
+        this.textMatchResult =
+          "匹配位置：" + regex.lastIndex + "\r\n匹配结果：" + result[0];
+      }
+    }
   },
 
   created() {
